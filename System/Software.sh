e8891e1dd961724567c8f852427683b84d072fc1
Saulwonderfulman <67410761+Saulwonderfulman@users.noreply.github.com>
2026-02-01 15:25:57 +0800

    Update software installation list in Software.sh

--- System/Software.sh
+++ System/Software.sh
@@ -1,6 +1,2 @@
-su -
-apt update
-apt upgrade -y
-apt install -y clash-verge vim vlc keepassxc syncthing adb freecad flameshot neofetch meld     
-apt install wemeet audacity sunloginclient
-#dpkg -i wechat 
+apt install -y clash-verge vim vlc keepassxc syncthing adb freecad flameshot neofetch btop supertuxkart 盒子 fceux cutecom vscodium pulseview gimp     
+#dpkg -i wechat wemeet sunloginclient
