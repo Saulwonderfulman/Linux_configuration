10cba506fe659a866bc9727304925335bfb6fcb4
Saulwonderfulman <67410761+Saulwonderfulman@users.noreply.github.com>
2026-02-04 16:25:46 +0800

    Update proprietary software installation list
    Added STM32CubeIDE to the list of proprietary software installations.

--- System/Software.sh
+++ System/Software.sh
@@ -4,7 +4,7 @@ apt install -y clash-verge vim vlc keepassxc syncthing adb freecad flameshot neo
 # 添加Flathub 仓库
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-# 专有软件
-flatpak install flathub com.tencent.WeChat com.qq.QQ com.tencent.wemeet com.github.tchx84.Flatseal io.github.flattool.Warehouse com.github.gmg137.netease-cloud-music-gtk
+# 专有软件（微信、QQ、腾讯会议、STM32CubeIDE）（Flatseal、Warehouse、cloud-music属于自由软件）
+flatpak install flathub com.tencent.WeChat com.qq.QQ com.tencent.wemeet com.github.tchx84.Flatseal io.github.flattool.Warehouse com.github.gmg137.netease-cloud-music-gtk com.st.STM32CubeIDE
 
 
