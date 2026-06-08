d90ae140567e5a446c147d18040bef2bdd557c81
Saulwonderfulman <67410761+Saulwonderfulman@users.noreply.github.com>
2026-06-08 15:37:07 +0800

    Update Software.sh

--- System/Software.sh
+++ System/Software.sh
@@ -5,6 +5,6 @@ apt install -y clash-verge vim vlc keepassxc syncthing nextcloud-desktop adb fre
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # 专有软件（微信、QQ、腾讯会议、STM32CubeIDE）（Flatseal、Warehouse、cloud-music属于自由软件）
-flatpak install flathub com.tencent.WeChat com.qq.QQ com.tencent.wemeet com.github.tchx84.Flatseal io.github.flattool.Warehouse com.github.gmg137.netease-cloud-music-gtk com.st.STM32CubeIDE
+flatpak install flathub com.tencent.WeChat com.qq.QQ com.tencent.wemeet com.github.tchx84.Flatseal io.github.flattool.Warehouse com.github.gmg137.netease-cloud-music-gtk com.st.STM32CubeIDE cn.wps.wps_365
 
 
